5429867a80ba2aa3d927b44935d581792d7b2f4f
isumalatov <isumalatov@gmail.com>
2024-05-03 16:33:27 +0200

    cleaning template

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,18 +1,17 @@
 export const metadata = {
-  title: 'Home - Stellar',
-  description: 'Page description',
-}
+  title: "Home - Stellar",
+  description: "Page description",
+};
 
-import Hero from '@/components/hero'
-import Clients from '@/components/clients'
-import Features from '@/components/features'
-import Features02 from '@/components/features-02'
-import Features03 from '@/components/features-03'
-import TestimonialsCarousel from '@/components/testimonials-carousel'
-import Features04 from '@/components/features-04'
-import Pricing from './pricing-section'
-import Testimonials from '@/components/testimonials'
-import Cta from '@/components/cta'
+import Hero from "@/components/hero";
+import Clients from "@/components/clients";
+import Features from "@/components/features";
+import Features02 from "@/components/features-02";
+import Features03 from "@/components/features-03";
+import TestimonialsCarousel from "@/components/testimonials-carousel";
+import Features04 from "@/components/features-04";
+import Testimonials from "@/components/testimonials";
+import Cta from "@/components/cta";
 
 export default function Home() {
   return (
@@ -20,13 +19,6 @@ export default function Home() {
       <Hero />
       <Clients />
       <Features />
-      <Features02 />
-      <Features03 />
-      <TestimonialsCarousel />
-      <Features04 />
-      <Pricing />
-      <Testimonials />
-      <Cta />
     </>
-  )
+  );
 }
